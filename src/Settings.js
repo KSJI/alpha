fa101fe347d42c479716c0b1dc33f11fbbc6ee81
Kyle Simpson <kes97@uw.edu>
2018-05-22 15:58:36 -0700

    Show placeholders

--- src/Settings.js
+++ src/Settings.js
@@ -4,19 +4,38 @@ import { DisplayHeader } from './DisplayHeader';
 import './Settings.css';
 
 export class DisplayEditAccountSettings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            weight: 0
+        }
+    }
+
+    componentDidMount() {
+        let user =  firebase.auth().currentUser;
+        console.log(user);
+        this.setState({
+            email: "email", //user.email
+            password:"the", //user.password
+            weight:123 //user.weight
+        })
+    }
+
     render() {
         return (
             <div>
                 <DisplayHeader />
                 <div className="settings">
                     <div className="email">
-                        <p className="text">Email</p> <input type="text"></input>
+                        <p className="text">Email</p> <input type="text" placeholder={this.state.email}></input>
                     </div>
                     <div className="password">
-                        <p className="text">Change Password</p> <input type="text"></input>
+                        <p className="text">Change Password</p> <input type="text" placeholder={this.state.password}></input>
                     </div>
                     <div className="weight">
-                        <p className="text">Weight</p> <input type="text"></input> lbs
+                        <p className="text">Weight</p> <input type="text" placeholder={this.state.weight}></input> lbs
                     </div>
 
                     <div className="save">
